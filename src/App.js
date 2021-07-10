b071e84ccd7f69db8535a7b5f814689a83e78f56
BrianJenney <brianjenney83@gmail.com>
2021-07-10 09:27:50 -0700

    initial commit;

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { SignIn } from './Pages/SignIn';
+import { SongUpload } from './Pages/SongUpload';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route path="/artist/upload">
+                        <SongUpload />
+                    </Route>
+                    <Route path="/">
+                        <SignIn />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
+};
 
 export default App;
---
+++ src/Pages/SongUpload/index.js
@@ -0,0 +1,1 @@
+export { default as SongUpload } from './SongUpload.container';
