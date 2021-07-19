2938f5a3ce55b125c82749e0c8165515c9d428d3
BrianJenney <brianjenney83@gmail.com>
2021-07-18 17:53:58 -0700

    fix url format

--- src/Pages/Music/Music.js
+++ src/Pages/Music/Music.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import ReactAudioPlayer from 'react-audio-player';
 
+const formatUrl = (url) => {
+    const segements = url.split('/');
+    segements[segements.length - 1] = encodeURIComponent(
+        segements[segements.length - 1]
+    );
+    return segements.join('/');
+};
+
 const Music = ({ songs = [] }) => (
     <div className="h-screen bg-gray-900 overflow-hidden flex flex-col items-center content-center ">
         <div className="mt-32 divide-y-2 divide-purple-300 divide-solid">
-            {songs.map((song) => (
-                <div className="pb-4 py-4">
+            {songs.map((song, i) => (
+                <div key={`${i}_${song.url}`} className="pb-4 py-4">
                     <p className="text-white uppercase">{song.artistName}</p>
                     <div className="flex items-center space-x-4">
                         <img
                             height="100px"
                             width="100px"
                             alt="song art"
-                            src={song?.songCoverUrl}
+                            src={formatUrl(song?.songCoverUrl)}
                         />
                         <p className="text-white uppercase">{song.title}</p>
                         <ReactAudioPlayer
                             autoPlay={false}
                             controls
-                            src={song.url}
-                            preload={false}
+                            src={formatUrl(song.url)}
+                            preload="auto"
                         />
                     </div>
                 </div>
