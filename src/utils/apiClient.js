32d13b3af663413472dbce795a1bfda801d77da8
BrianJenney <brianjenney83@gmail.com>
2021-10-03 12:00:45 -0700

    use correct urls

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
+const PROD_URL =
+    'https://fjsu16onvd.execute-api.us-east-1.amazonaws.com/production/api/';
+const STAGE_URL =
+    'https://4mxwytmbdh.execute-api.us-east-1.amazonaws.com/dev/api/';
+
 const getBaseUrl = () => {
     const urlsByEnv = {
-        production:
-            'https://4mxwytmbdh.execute-api.us-east-1.amazonaws.com/prod/',
-        dev: 'https://4mxwytmbdh.execute-api.us-east-1.amazonaws.com/dev/api/',
-        stage: 'https://4mxwytmbdh.execute-api.us-east-1.amazonaws.com/dev/api/',
+        production: PROD_URL,
+        dev: STAGE_URL,
+        stage: STAGE_URL,
     };
     const env = process.env.NODE_ENV;
     return urlsByEnv[env] || 'http://localhost:5000/api/';
