08221e68e6fcf055edffe14c1dbc6d53a3b7f146
BrianJenney <brianjenney83@gmail.com>
2021-11-15 15:39:56 -0800

    remove service worker stuff

--- public/css.js
+++ public/css.js
@@ -1,34 +1,60 @@
 (function () {
-    window.onload = () => {
-        window
-            .fetch(
-                'http://localhost:5000/api/styles/getconfig?spaceid=123ABC&isPreview=false'
-            )
-            .then((response) => response.json())
-            .then((data) => {
-                let myStringOfstyles = '';
+    window.pspx = {
+        init: function ({ spaceid, isPreview }) {
+            if (!spaceid) {
+                throw new Error('missing space id');
+            }
+            window
+                .fetch(
+                    'http://localhost:5000/api/styles/getconfig?spaceid=' +
+                        spaceid +
+                        '&isPreview=' +
+                        isPreview
+                )
+                .then((response) => response.json())
+                .then((data) => {
+                    let myStringOfstyles = '';
 
-                if (
-                    Array.isArray(data.styleConfig.styles) &&
-                    data.styleConfig.styles.length
-                ) {
-                    data.styleConfig.styles.forEach((rule) => {
-                        myStringOfstyles += `${rule.element} { ${rule.styles
-                            .map((styleStr) => {
-                                return `${styleStr} !important; `;
-                            })
-                            .join(' ')} } `;
-                    });
-                }
+                    if (
+                        Array.isArray(data.styleConfig.styles) &&
+                        data.styleConfig.styles.length
+                    ) {
+                        data.styleConfig.styles.forEach((rule) => {
+                            const currentRule = `${rule.element} { ${rule.styles
+                                .map((styleStr) => {
+                                    return `${styleStr} !important; `;
+                                })
+                                .join(' ')} } `;
 
-                const head =
-                        document.head ||
-                        document.getElementsByTagName('head')[0],
-                    style = document.createElement('style');
+                            if (rule.maxWidth || rule.minWidth) {
+                                const screenSizeRule = rule.maxWidth
+                                    ? 'max-width'
+                                    : 'min-width';
+                                myStringOfstyles += `@media only screen and (${screenSizeRule}: ${
+                                    rule.minWidth || rule.maxWidth
+                                }px) {
+                                    ${currentRule}
+                                }`;
+                            } else {
+                                myStringOfstyles += currentRule;
+                            }
+                        });
+                    }
 
-                head.appendChild(style);
-                style.type = 'text/css';
-                style.appendChild(document.createTextNode(myStringOfstyles));
-            });
+                    const head =
+                            document.head ||
+                            document.getElementsByTagName('head')[0],
+                        style = document.createElement('style');
+
+                    head.appendChild(style);
+                    style.type = 'text/css';
+                    style.appendChild(
+                        document.createTextNode(myStringOfstyles)
+                    );
+                })
+                .catch((err) => {
+                    throw new Error('pspx failed to load');
+                });
+        },
     };
 })();
